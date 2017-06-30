8bd8329b3cc6213c1082d1944849c1ee355544ac
logan lembke <logan@blackhillsinfosec.com>
2017-06-30 13:13:12 -0600

    Reduce verbosity, add dialinsecure

--- secure-mgo.go
+++ secure-mgo.go
@@ -11,30 +11,30 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
-//MongoAuthMechanism represents a MongoDB authentication mechanism
-type MongoAuthMechanism string
+//AuthMechanism represents a MongoDB authentication mechanism
+type AuthMechanism string
 
 const (
 	//ScramSha1 = SCRAM-SHA-1
-	ScramSha1 MongoAuthMechanism = "SCRAM-SHA-1"
+	ScramSha1 AuthMechanism = "SCRAM-SHA-1"
 	//MongoCR = MONGODB-CR
-	MongoCR MongoAuthMechanism = "MONGODB-CR"
+	MongoCR AuthMechanism = "MONGODB-CR"
 	//PlainAuth = PLAIN
-	PlainAuth MongoAuthMechanism = "PLAIN"
+	PlainAuth AuthMechanism = "PLAIN"
 	//X500 = MONGODB-X500
-	X500 MongoAuthMechanism = "MONGODB-X500"
+	X500 AuthMechanism = "MONGODB-X500"
 	//GssAPI = GSSAPI
-	GssAPI MongoAuthMechanism = "GSSAPI"
+	GssAPI AuthMechanism = "GSSAPI"
 	//None is used to show that authentication is disabled
-	None MongoAuthMechanism = ""
+	None AuthMechanism = ""
 )
 
-var authMechanisms = []MongoAuthMechanism{ScramSha1, MongoCR, PlainAuth, X500, GssAPI, None}
+var authMechanisms = []AuthMechanism{ScramSha1, MongoCR, PlainAuth, X500, GssAPI, None}
 
 //Dial dials a MongoDB server with the provided MongoDB connection string,
-//as supplied to the mongo utility, an auth mechanism (see MongoAuthMechanism),
+//as supplied to the mongo utility, an auth mechanism (see AuthMechanism),
 //and a TLS configuration
-func Dial(connString string, authMechanism MongoAuthMechanism, conf *tls.Config) (*mgo.Session, error) {
+func Dial(connString string, authMechanism AuthMechanism, conf *tls.Config) (*mgo.Session, error) {
 	dialInfo, err := mgo.ParseURL(connString)
 	if err != nil {
 		return nil, err
@@ -55,9 +55,31 @@ func Dial(connString string, authMechanism MongoAuthMechanism, conf *tls.Config)
 	return mgo.DialWithInfo(dialInfo)
 }
 
-//ParseMongoAuthMechanism parses a string representation of a MongoDB
-//authentication mechanism and returns the MongoAuthMechanism which matches
-func ParseMongoAuthMechanism(mechanism string) (MongoAuthMechanism, error) {
+//DialInsecure dials a MongoDB server with the provided MongoDB connection string,
+//as supplied to the mongo utility, an auth mechanism (see AuthMechanism).
+//Note: this method does not encrypt any information placed on the wire,
+//including authentication details
+func DialInsecure(connString string, authMechanism AuthMechanism) (*mgo.Session, error) {
+	dialInfo, err := mgo.ParseURL(connString)
+	if err != nil {
+		return nil, err
+	}
+
+	dialInfo.Timeout = 5 * time.Second
+
+	if authMechanism == None {
+		dialInfo.Username = ""
+		dialInfo.Password = ""
+	}
+
+	dialInfo.Mechanism = string(authMechanism)
+
+	return mgo.DialWithInfo(dialInfo)
+}
+
+//ParseAuthMechanism parses a string representation of a MongoDB
+//authentication mechanism and returns the AuthMechanism which matches
+func ParseAuthMechanism(mechanism string) (AuthMechanism, error) {
 	mechanism = strings.ToUpper(mechanism)
 	strings.Map(func(r rune) rune {
 		if unicode.IsSpace(r) {
